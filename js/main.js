104f3175428a6f7742726184f2c5f35cab1f03e1
Rothy <jahanrothy58@gamil.com>
2022-05-16 17:07:39 +0600

    version 1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+<script type="text/javascript">
+		var navLinks = document.getElementById('navLinks');
+		 
+		 function showMenu(){
+		 	navLinks.style.right = "0";
+		 }
+		  function hideMenu(){
+		 	navLinks.style.right = "-200px";
+		 }
+	</script>
